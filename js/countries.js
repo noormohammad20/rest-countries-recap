d0d5d7d1b9d1b67dfa17263e1698665baae8d571
noormohammad20 <mdsaim204@gmail.com>
2022-03-15 17:26:54 +0600

    first commit

---
+++ js/countries.js
@@ -0,0 +1,48 @@
+const loadCountries = () => {
+    fetch(`https://restcountries.com/v3.1/all`)
+        .then(res => res.json())
+        .then(data => displayCountries(data))
+}
+
+const displayCountries = countries => {
+    // console.log(countries)
+    const countriesHTML = countries.map(country => getCountryHTML(country))
+    // console.log(countriesHTML)
+    const container = document.getElementById('countries')
+    container.innerHTML = countriesHTML.join(' ')
+}
+
+// option 2 
+const getCountryHTML = ({ name, flags, area, region }) => {
+    return `
+<div class="country"> 
+<h2>${name.common}</h2>
+<p>Area:${area}</p>
+<p>Region:${region}</p>
+<img src="${flags.png}">
+</div>
+`
+}
+// option 1
+// const getCountryHTML = country => {
+//     const { name, flags } = country
+//     return `
+// <div class="country"> 
+// <h2>${name.common}</h2>
+// <img src="${flags.png}">
+// </div>
+// `
+// }
+
+// original 
+// const getCountryHTML = country => {
+//     return `
+// <div class="country"> 
+// <h2>${country.name.common}</h2>
+// <img src="${country.flags.png}">
+// </div>
+// `
+// }
+
+
+loadCountries()
